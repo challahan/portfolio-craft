9c529ce0a1818583848c4dc884b830979b9082e8
Christopher Hallahan <challahan@gmail.com>
2016-12-31 00:45:10 -0500

    fix analytics

--- html/scripts/scripts.js
+++ html/scripts/scripts.js
@@ -10,7 +10,7 @@ if(e&&1===a.nodeType)while(c=e[d++])a.removeAttribute(c)}}),hb={set:function(a,b
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//challahan.com/scripts/analytics.js','ga');
+})(window,document,'script','//chrishallahan.com/scripts/analytics.js','ga');
 
   ga('create', 'UA-1380129-2', 'auto');
   ga('send', 'pageview');

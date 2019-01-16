1cc64036145c9ed0f91a4a0b820f8972a03cf83d
Christopher Hallahan <challahan@gmail.com>
2019-01-16 15:37:30 -0500

    Update Craft to v. 3.1

--- craft/vendor/craftcms/cms/src/elements/db/UserQuery.php
+++ craft/vendor/craftcms/cms/src/elements/db/UserQuery.php
@@ -10,6 +10,7 @@ namespace craft\elements\db;
 use Craft;
 use craft\db\Query;
 use craft\db\QueryAbortedException;
+use craft\db\Table;
 use craft\elements\User;
 use craft\helpers\Db;
 use craft\models\UserGroup;
@@ -30,6 +31,11 @@ class UserQuery extends ElementQuery
 {
     // Properties
     // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    protected $defaultOrderBy = ['users.username' => SORT_ASC];
 
     // General parameters
     // -------------------------------------------------------------------------
@@ -140,11 +146,6 @@ class UserQuery extends ElementQuery
      */
     public function __construct($elementType, array $config = [])
     {
-        // Default orderBy
-        if (!isset($config['orderBy'])) {
-            $config['orderBy'] = 'users.username';
-        }
-
         // Default status
         if (!isset($config['status'])) {
             $config['status'] = [User::STATUS_ACTIVE];
@@ -265,7 +266,7 @@ class UserQuery extends ElementQuery
         } else if ($value !== null) {
             $this->groupId = (new Query())
                 ->select(['id'])
-                ->from(['{{%usergroups}}'])
+                ->from([Table::USERGROUPS])
                 ->where(Db::parseParam('handle', $value))
                 ->column();
         } else {
@@ -591,7 +592,7 @@ class UserQuery extends ElementQuery
         if ($this->groupId) {
             $userIds = (new Query())
                 ->select(['userId'])
-                ->from(['{{%usergroups_users}}'])
+                ->from([Table::USERGROUPS_USERS])
                 ->where(Db::parseParam('groupId', $this->groupId))
                 ->column();
 
@@ -674,7 +675,7 @@ class UserQuery extends ElementQuery
             // Convert it to the actual permission ID, or false if the permission doesn't have an ID yet.
             $this->can = (new Query())
                 ->select(['id'])
-                ->from(['{{%userpermissions}}'])
+                ->from([Table::USERPERMISSIONS])
                 ->where(['name' => strtolower($this->can)])
                 ->scalar();
         }
@@ -684,7 +685,7 @@ class UserQuery extends ElementQuery
             // Get the users that have that permission directly
             $permittedUserIds = (new Query())
                 ->select(['userId'])
-                ->from(['{{%userpermissions_users}}'])
+                ->from([Table::USERPERMISSIONS_USERS])
                 ->where(['permissionId' => $this->can])
                 ->column();
 
